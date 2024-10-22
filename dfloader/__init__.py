ff1aa5577526f279ce54fcc5567f4dfa45736fa3
Ashok Cutkosky <ashok@cutkosky.com>
2024-10-22 12:10:55 -0400

    added repeat_count and seen_count columns, some better defaults on dask and column names

--- dfloader/__init__.py
+++ dfloader/__init__.py
@@ -5,10 +5,19 @@
 import numpy as np
 import pandas as pd
 from typing import Sequence, Optional, Union, List
-import dask.dataframe as dd
 import collections
 import loadit
 
+try:
+    import dask.dataframe as dd
+except ImportError:
+    # dask is not installed, make up a 
+    # dummy class to avoid errors when checking
+    # for dask functionality later on.
+    class dd:
+        def __init__(self):
+            self.DataFrame = None
+        
 
 def drop_non_numeric_columns(df: Union[pd.DataFrame, dd.DataFrame]):
     # mostly copied from chatgpt :)
@@ -47,7 +56,7 @@ class Dataset(collections.abc.Sequence):
         shuffle_seed: Optional[int] = None,
         skip_index_check: bool = False,
         return_type: str = 'numpy',
-        columns: List[str] = [],
+        columns: Optional[List[str]] = None,
     ):
 
 
@@ -55,12 +64,23 @@ class Dataset(collections.abc.Sequence):
             raise ValueError(f"Unknown return type: {return_type}")
         self.return_type = return_type
 
-        self.columns = columns + ['__mask__']
+        #  validate columns argument/generate defaults when needed.
+        extra_columns = ['__valid_data__', '__repeat_count__', '__seen_count__']
+
         if is_dataframe(df):
-            if columns != []:
+            if columns is not None:
                 if not same_lists(columns, list(df.columns)):
                     raise ValueError("provided columns argument is not the same as df.columns")
-            self.columns  = list(df.columns) + ['__mask__']
+            self.columns  = list(df.columns)
+        else:
+            if columns is None:
+                columns = list(range(df.shape[1]))
+
+        for c in extra_columns:
+            if c in columns:
+                raise ValueError(f"original columns cannot already contain {c}")
+
+        self.columns = columns + extra_columns
 
         if isinstance(df, np.ndarray) and return_type == 'dict':
             if  len(columns) != df.shape[-1]:
@@ -68,9 +88,9 @@ class Dataset(collections.abc.Sequence):
 
 
         if is_dataframe(df):
-            self.mask_dtype = bool
+            self.valid_data_dtype = bool
         else:
-            self.mask_dtype = df.dtype
+            self.valid_data_dtype = df.dtype
 
         self.df = df
 
@@ -114,13 +134,76 @@ class Dataset(collections.abc.Sequence):
 
 
         # Now, let us discuss the extra K columns.
-        # Currently  K=1 (maybe it will be bigger in later version...)
-        
+        # Currently  K=3 (maybe it will be bigger in later version...)
+
+
+        ### EXTRA COLUMN 1: __valid_data__ ###
         # The first extra column is the "valid_data" mask column.
         # We set
         # loader[n, b, C, t] = 1 if [n,b, :, t] corresponds to valid indices in df
         # (these indices are df[S + (n*B + b)*stride + t - T + 1, :] ).
         # Otherwise, loader[n ,b, C, t] = 0
+        # That is, this  column just checks if S + (n*B + b)*stride + t - T +1 is
+        # (1) non-negative, and (2) less than len(df)
+
+
+        ### EXTRA COLUMN 2: __repeat_count__ ###
+        # The second extra column is the "repeat_count" column. Since the stride
+        # may not equal the context length, some rows may repeat. We will generate
+        # a new column that tells how many times each row repeats. We set the  value to zero
+        # when the data is out-of-range in the original df (i.e.  when __valid_data__ is False).
+        # Let's do the calculution carefully.
+        # Recall that loader[n, b, :, t] corresponds to
+        # df[S +(n*B +b)*stride +  t -T + 1, :].
+        # Let us write 
+        # F := S + (n*B + b)*stride  + t - T +1
+        #
+        # Therefore loader[n, b, C+1, t] should just count the number of possible
+        # values of n', b', t'  such that
+        # F = S + (n' * B + b') *stride  + t' - T + 1
+        #
+        # To do this count, first observe that for a given n',b', there is at most
+        # one possible value for t'. 
+        # Further, the values of (n'*B + b') that result in "valid" data from df 
+        # (that is, for which S + (n' * B + b') *stride + t' - T +1 \in [0, L-1] for some t')
+        # take on all integers from max(0, -floor(S/stride) ) to  floor((L - S + T - 1)/stride) exactly once each,
+        # where we recall that df has shape [L, C]
+        
+        # Next for a given value of p = n'*B+b', in order for there to be a possible value for t', we must have:
+        # S + p*stride - T + 1 <= F
+        # AND
+        # S +  p *  stride >= F
+        # Equivalently:
+        # p * stride \in [F - S, F - S + T - 1]
+        # So, p must satisfy:
+        # p \in [ceil( (F-S)/stride ), floor( (F -S + T  -1)/stride ) ]
+        #     = [n*B+b + ceil((t-T+1)/stride), n*B+b + floor(t/stride)]
+        # Combining both restrictions on p:
+        # p \in [max(n*B+b + ceil((t-T+1)/stride), 0, -floor(S/stride)),  min(n*B+b+floor(t/stride), floor((L-S+T-1)/stride))]
+        # So finally, the number  of  possible p values (and therefore the number of duplicates) is:
+        # min(n*B+b + floor(t/stride), floor((L-S+T-1)/stride)) - max(n*B+b + ceil((t-T+1)/stride), 0, -floor(S/stride)) + 1
+
+
+        ### EXTRA COLUMN 3: __seen_count__ ###
+        # The third extra column counts how many times this row has appeared so far when iterating through the
+        # data in order.
+        # That is, if we iterate through  loader[n, b, :, t] by first cycling through b from 0 to B-1 and then incrementing
+        # n (which will go through the data in the same order as iterating through df), then
+        # loader[n, b, C+2, t] will indicate the number of times this row has been produced before (counting the current iteration).
+        # So, loader[n, b, C+2, t] will have minimum value 1 and maximum value equal to loader[n, b, C+1,  t].
+        # We set the  value to zero when the data is out-of-range in the original df (i.e.  when __valid_data__ is False).
+
+        # This can be used to identify specific occurances of each row. For example, we might wish to compute a loss only on labels for
+        # for which  this is the first time we will see the label.
+
+        # To calculate this value, we can reuse much of the algebra from computing extra column 2 (__repeat_count__):
+        # the minimum value of n'*B+b' that will generate the current row is:
+        # p_min = max(n*B+b + ceil((t-T+1)/stride), 0, -floor(S/stride))
+        # All values of  n'*B+b' that are between the current value and p_min must have appeared exactly once each previously, so:
+        # loader[n, b, C+2, t] = n*B+b - p_min + 1
+
+        
+        
 
         
         if isinstance(df, np.ndarray):
@@ -152,8 +235,12 @@ class Dataset(collections.abc.Sequence):
         self.shuffled_indices = self.rng.permutation(self.length * self.batch_size)
 
     def __getitem__(self, idx: int):
-        if idx >= len(self):
+        if idx < 0:
+            idx = len(self) + idx
+        
+        if idx >= len(self) or idx < 0:
             raise IndexError
+        
 
        
         # This next line is unfortunately a bit "clever".
@@ -169,14 +256,35 @@ class Dataset(collections.abc.Sequence):
         context_indices  = np.arange(0, self.context_length)
         batch_indices = np.arange(idx*self.batch_size, idx*self.batch_size + self.batch_size)
         if self.shuffle_seed is not None:
-            # print("batch indices: ",batch_indices, "suffled indices: ",self.shuffled_indices)
             batch_indices = self.shuffled_indices[batch_indices]
-        virtual_df_indices = self.start_idx  - self.context_length + 1 + (context_indices.reshape(1,-1) + self.stride * batch_indices.reshape(-1,1))
 
-        # mask_column[b, t] = 0 whenever start_idx + (idx * batch_size + b -1) * stride + t - context_length + 1
+        context_indices = context_indices.reshape(1, -1)
+        batch_indices = batch_indices.reshape(-1, 1)
+        virtual_df_indices = self.start_idx  - self.context_length + 1 + (context_indices + self.stride * batch_indices)
+
+
+        ### Computing extra columns ###
+
+        ### __valid__data__ ###
+        # valid_data[b, t] = 0 whenever start_idx + (idx * batch_size + b -1) * stride + t - context_length + 1
         # is not in >= 0 and < len(df).
-        mask_column = (virtual_df_indices >= 0) * (virtual_df_indices < len(self.df))
-        mask_column = mask_column.astype(self.mask_dtype).reshape((self.batch_size, 1, self.context_length))
+        valid_data = (virtual_df_indices >= 0) * (virtual_df_indices < len(self.df))
+        valid_data = valid_data.astype(self.valid_data_dtype).reshape((self.batch_size, 1, self.context_length))
+
+        ### __repeat_count__ ###
+        p_max = np.minimum(batch_indices + np.floor(context_indices/self.stride), np.floor((len(self.df)-self.start_idx + self.context_length - 1)/self.stride))
+
+        p_min = np.maximum(np.maximum(batch_indices + np.ceil((context_indices-self.context_length+1)/self.stride), -np.floor(self.start_idx/self.stride)), 0.0)
+
+        repeat_count = (p_max - p_min + 1).astype(int)
+        repeat_count = repeat_count.reshape((self.batch_size, 1, self.context_length)) * valid_data
+
+        ### __seen_count__ ###
+        seen_count = (batch_indices - p_min + 1).astype(int)
+        seen_count = seen_count.reshape((self.batch_size, 1, self.context_length)) * valid_data
+
+
+        
 
 
         logical_df_indices = np.clip(virtual_df_indices, a_min=0, a_max=len(self.df)-1)
@@ -199,7 +307,7 @@ class Dataset(collections.abc.Sequence):
         data = data.transpose((0,2,1))
             
 
-        data = np.concatenate((data, mask_column), axis=1)
+        data = np.concatenate((data, valid_data, repeat_count, seen_count), axis=1)
 
         if self.return_type == 'numpy':
             return data
